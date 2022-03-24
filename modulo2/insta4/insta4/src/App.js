bb73446455da2770d90b5304100b692e7061c5f5
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-03-24 18:52:13 -0300

    vacuous-power.surge.sh

--- modulo2/insta4/insta4/src/App.js
+++ modulo2/insta4/insta4/src/App.js
@@ -9,7 +9,7 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
-class App extends React.Component {
+/*class App extends React.Component {
   render() {
     return (
       <MainContainer>
@@ -34,4 +34,72 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App;*/
+
+
+
+class App extends React.Component {
+  state = {
+    pessoas:[
+      {
+    nomeUsuario:"paulinha",
+    fotoUsuario:"https://picsum.photos/50/50",
+    fotoPost:"https://picsum.photos/200/152"
+  },
+   {
+    nomeUsuario:"Luiz Marcelo",
+    fotoUsuario:"https://picsum.photos/50/45",
+    fotoPost:"https://picsum.photos/200/135"
+  },
+  {
+    nomeUsuario:"Labenu",
+    fotoUsuario:"https://picsum.photos/50/40",
+    fotoPost:"https://picsum.photos/200/148"
+  }
+],   
+    valorInputnomeUsuario: "",
+    valorInputfotoUsuario: "",
+    valorInputfotoPost: ""
+};
+
+
+render() {
+ 
+  const listaDeComponentes = this.state.pessoas.map((pessoa) => {
+    
+    return (
+      <Post
+        nomeUsuario ={pessoa.nomeUsuario}
+        fotoUsuario = {pessoa.fotoUsuario} 
+        fotoPost = {pessoa.fotoPost}
+      />
+    )
+  })
+
+  
+return (
+    <MainContainer>
+      {listaDeComponentes}
+      <div>
+          <input
+            value={this.state.valor}
+            onChange={this.onChangenomeUsuario}
+            placeholder={""}
+          />
+          <input
+            value={this.state.valor}
+            onChange={this.onChangefotoUsuario}
+            placeholder={""}
+          />
+          <input
+            value={this.state.valor}
+            onChange={this.onChangefotoUsuario}
+            placeholder={""}
+          />
+          <button onClick={this.adicionaPessoa}>Adicionar</button>
+        </div>
+        
+    </MainContainer>
+  );
+  }}
+  export default App;
