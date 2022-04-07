def878b485a488a3283aa4563cec53f3d3923d62
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-04-06 23:25:51 -0300

    apis-labenusers2

--- modulo2/apis-labenusers/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -1,105 +1,45 @@
 import React from 'react';
-import axios from 'axios';
-
+import TelaCadastro from './componentes/TelaCadastro';
+import TelaListaUsuarios from './componentes/TelaListaUsuarios';
 
 
 
 class App extends React.Component {
-  
   state = {
-    messages: [],
-    inputEmail: '',
-    inputNome: ''
+    telaAtual: "cadastro" //TelaCadastro, TelaListaUduarios ou pagina não encontrada
   }
-  
-  
-  componentDidMount=() =>
-  
-  
-  onChangeEmail = (event) => {
-    this.setState ({inputEmail: event.target.value})
-  }
-  
-  onChangeName = (event) => {
-    this.setState ({inputName: event.target.value})
-  }
-  
-  createMessagens = () => {
-    const body = {
-      name: this.state.inputNome,
-      email: this.state.inputEmail
+
+
+
+
+
+
+  escolheTela = () => {
+    switch (this.state.telaAtual) {
+      case "cadastro":
+        return <TelaCadastro irParaLista={this.irParaLista}/>
+      case "lista":
+        return <TelaListaUsuarios irParaCadastro={this.irParaCadastro}/>
+      default:
+        return <div>Erro Página não encontrada</div>
+
     }
-    const headers = {
-      headers: {
-        Authorization: "luiz-shaw"
-      }
-    };
-    /*const getAllPlaylists = () => {
-      const url =
-        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
-        axios
-    .post(url, body, headers)
-    .then((res) => {
-      //Aqui é a resposta da função caso dê tudo certo
-      console.log(res.data);
-    })
-    .catch((err) => {
-      //Aqui é a resposta da função caso dê errado
-      console.log(err.response.data);
-    });
-};
-getAllPlaylists();
-postPlaylist("Forroo");*/
+  }
+  irParaCadastro = () => {
+    this.setState({ telaAtual: "cadastro" })
+  }
+  irParaLista = () => {
+    this.setState({ telaAtual: "lista" })
+
+  }
 
 
-getUser = () => {
-  const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users?=77374f2e-d9dc-4343-89f9-dc8d60b0ad89"
-  axios.get(url,headers)
-  .them((resposta)=> {
-    this.setState ({
-      user:resposta.data
-    })
-  })
-}
-deletUser = (id) => {
-  const url = ""
-  axios.delete(url,headers)
-  this.getUser ()
-  
-}
-
-    
-  
-  
   render() {
-    
-    
-    
+
+
     return (
       <div>
-        <label>
-        Email
-        <button onClick={this.sendMessage}>Trocar de tela</button>
-        <input
-          placeholder="luiz_marccello@hotmail.com"
-          value={this.state.inputEmail}
-          onChange={this.onChange}
-        />
-        </label>
-        <label>
-          nome:
-          <input
-          placeholder="luiz_marccello@hotmail.com"
-          value={this.state.inputNome}
-          onChange={this.onChange}
-          onKeyPress={event => {
-            if (event.key === 'Enter') {
-              this.createMessagens()
-            }
-          }}
-          />
-        </label>
-        <button onClick={this.sendMessage}>Criar Usuario</button>
+        {this.escolheTela()}
       </div>
     );
   }
