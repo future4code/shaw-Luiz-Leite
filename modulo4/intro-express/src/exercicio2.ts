f22b764402363f88bd8c8905e4866fc69ee74205
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-05-30 23:52:03 -0300

    intro-express

--- modulo4/intro-express/src/exercicio2.ts
+++ modulo4/intro-express/src/exercicio2.ts
@@ -8,9 +8,27 @@ app.use(cors());
 
 app.get('/', function (req,res){
     
-})
+    type user = {
+        id: number,
+        name: string,
+        phone: number,
+        email: string,
+        website: string
+            
+    }
+
+    let userList: user[] = [
+{id:1,
+name:"luiz",
+phone:6796722233,
+email:"luiz_marccello@hotmail.com",
+website: "www.luiz.com.br"}
+
+    ]
+    res.send(userList);
+});
 
 app.listen(3003,function () {
 console.log ("meu servidor está rodando na porta 3003")
 
-})
+});
