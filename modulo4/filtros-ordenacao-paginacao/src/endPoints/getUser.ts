52b4bc65d37e801fe54f1c45993ae2f7543e2143
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-06-15 17:56:47 -0300

    serviços-back

---
+++ modulo4/filtros-ordenacao-paginacao/src/endPoints/getUser.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+
+
+//a) Crie uma cópia do endpoint acima, e altere-o para que 
+//ele possa receber um parâmetro de filtragem por nome. 
+//Este nome deve ser enviado por query params.
+
+export const getUser = async(req: Request,res: Response): Promise<void> =>{
+    try {
+       const users = await selectAllUsers()
+ 
+       if(!users.length){
+          res.statusCode = 404
+          throw new Error("No recipes found")
+       }
+ 
+       res.status(200).send(users)
+       
+    } catch (error) {
+       console.log(error)
+       res.send(error.message || error.sqlMessage)
+    }
+ }
