b03f1287cc6bcf306acd1d46ca5631db0873863e
Luiz Marcelo <luiz_marccello@hotmail.com>
2022-07-19 17:13:24 -0300

    labefood

---
+++ modulo6/Projeto-labefood-case1/caselabefood/src/routes/Coordnator.js
@@ -0,0 +1,16 @@
+export const gotToLogin = (navigate) => {
+    navigate('/')
+}
+
+export const gotToFeed = (navigate) => {
+    navigate('/feed')
+}
+
+export const gotToSingUp = (navigate) => {
+    navigate('/signup')
+}
+
+export const gotToSingUpAdress = (navigate) => {
+    navigate('/signup/adress')
+}
+
---
+++ modulo6/Projeto-labefood-case1/caselabefood/src/routes/Router.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/login/Login";
+import Signup from "../pages/singup/SingUp";
+import SignupAdress from "../pages/singupadress/SingupAdress";
+import Feed from "../pages/feed/Feed";
+import Restaurant from "../pages/restaurant/Restaurant";
+import Profile from "../pages/profile/Profile";
+import Cart from "../pages/cart/Cart"
+
+
+
+
+
+const Router = () =>{
+    return(
+        <BrowserRouter>
+            <Routes>
+            <Route index element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/signup/adress" element={<SignupAdress/>}/>
+            <Route path="/feed" element={<Feed/>}/>
+            <Route path="/feed/restaurant" element={<Restaurant/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/cart" element={<Cart/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Router
